0b3a51acc7137dc3d02edfb0cf91d30ab6d69d1f
Víctor Sánchez del Río <victor@plign.com>
2020-12-06 14:37:50 +0100

    Added javadoc

---
+++ javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.victorsanchez.exercise"},{"l":"com.victorsanchez.exercise.dto"},{"l":"com.victorsanchez.exercise.persistence"},{"l":"com.victorsanchez.exercise.persistence.entity"},{"l":"com.victorsanchez.exercise.persistence.repository"},{"l":"com.victorsanchez.exercise.utils"}];updateSearchResults();
